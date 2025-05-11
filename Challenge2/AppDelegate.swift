9028c12b8c35988bd524df69ddc01ba2fbb8319d
MoonGoblin <bregas.sat1@gmail.com>
2025-05-11 19:18:10 +0800

    Initial Commit

---
+++ Challenge2/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Challenge2
+//
+//  Created by Bregas Satria Wicaksono on 11/05/25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
